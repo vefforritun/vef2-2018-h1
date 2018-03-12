28cc9e8a9e4006448c39ef1a84d6707dcd2ef6e7
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2018-03-12 21:04:44 +0000

    Add schema for category and books

--- schema.sql
+++ schema.sql
@@ -5,3 +5,17 @@ CREATE TABLE Users (
   name text NOT NULL,
   image text
 );
+
+CREATE TABLE Categories (
+  id serial PRIMARY KEY,
+  category varchar(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE Books (
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  isbn varchar(13) UNIQUE NOT NULL,
+  author text,
+  description text,
+  category int NOT NULL
+);
